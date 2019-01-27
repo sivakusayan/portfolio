e5ca414b29c46bb310713703e642091aba106250
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-01-27 16:26:24 -0500

    Copy for graham Scan project

--- src/__copy__/projects/GRAHAM_SCAN_COPY.js
+++ src/__copy__/projects/GRAHAM_SCAN_COPY.js
@@ -1,5 +1,10 @@
 const GRAHAM_SCAN_COPY = {
-
+  title: 'Visual Graham Scan',
+  subtitle: 'A geometric playground for the Graham Scan algorithm',
+  description: 'Visual Graham Scan is a responsive, educational web app that gives a step-by-step playthrough of an algorithm to compute convex hulls. This project was originally a command line tool I wrote for my Computational Geometry class. Since algorithm visualizations are one of the coolest things in the world, I figured that I had to give my classwork a little upgrade.',
+  technologies: ['react', 'redux', 'jest', 'enzyme'],
+  livelink: 'https://www.visualgrahamscan.com/',
+  gitLink: 'https://github.com/sivakusayan/visual-graham-scan/tree/master/src',
 };
 
 export default GRAHAM_SCAN_COPY;
