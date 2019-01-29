96c403780c78ba6d1380e9d18e9d6b546296881f
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-01-29 09:04:25 -0500

    Added copy for schedule maker project. Included project in home page

---
+++ src/__copy__/projects/SCHEDULE_MAKER_COPY.js
@@ -0,0 +1,10 @@
+const GRAHAM_SCAN_COPY = {
+  title: 'Schedule Maker',
+  subtitle: 'A weekly planner for busy people.',
+  description: 'One of my first JavaScript applications, originally written in ES5 and later upgraded to ES6. This project taught me a lot of important programming concepts, such as encapsulation, state, and efficient manipulation of the DOM.',
+  technologies: ['Javascript', 'SASS'],
+  liveLink: 'https://schedulemaker.netlify.com/',
+  gitLink: 'https://github.com/sivakusayan/schedule_app',
+};
+
+export default GRAHAM_SCAN_COPY;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import grahamScanVideo from '../assets/videos/grahamScanPreview.min.mp4'
+import grahamScanVideo from '../assets/videos/grahamScanPreview.min.mp4';
+import scheduleMakerVideo from '../assets/videos/scheduleMakerPreview.min.mp4';
 
 import Layout from '../components/layout'
 import Project from '../components/Project';
 
 import GRAHAM_SCAN_COPY from '../__copy__/projects/GRAHAM_SCAN_COPY';
+import SCHEDULE_MAKER_COPY from '../__copy__/projects/SCHEDULE_MAKER_COPY';
 
 const IndexPage = () => (
   <Layout>
     <Project video={grahamScanVideo} {...GRAHAM_SCAN_COPY} />
+    <Project video={scheduleMakerVideo} {...SCHEDULE_MAKER_COPY} />
   </Layout>
 )
 
