3e3b0ca870d6f28712649c8ba94ca7453579790a
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-02-11 10:58:49 -0500

    Background is now only in header. Added EOVIS copy

---
+++ src/__copy__/projects/EOVIS_COPY.js
@@ -0,0 +1,10 @@
+const EOVIS_COPY = {
+  title: 'EOVIS',
+  subtitle: 'An interactive map displaying natural disasters.',
+  description: 'Built with the MERN stack, the application fetches data from an API to create an ever growing database of events. Using this data, a map of events is generated for the user to explore. If they like, the user can also search for events by location, date, and category.',
+  technologies: ['React', 'Redux', 'Mapbox', 'Node.js'],
+  liveLink: 'https://murmuring-shelf-63300.herokuapp.com/',
+  gitLink: 'https://github.com/sivakusayan/eovis',
+};
+
+export default EOVIS_COPY;
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Reveal from 'react-reveal/Reveal';
 
 import Project from '../components/Project';
+
 import grahamScanVideo from '../assets/videos/grahamScanPreview.min.mp4';
 import scheduleMakerVideo from '../assets/videos/scheduleMakerPreview.min.mp4';
+import eovisVideo from '../assets/videos/eovisPreview.min.mp4';
+
+import EOVIS_COPY from '../__copy__/projects/EOVIS_COPY';
 import GRAHAM_SCAN_COPY from '../__copy__/projects/GRAHAM_SCAN_COPY';
 import SCHEDULE_MAKER_COPY from '../__copy__/projects/SCHEDULE_MAKER_COPY';
 
@@ -11,6 +15,7 @@ const Projects = () => (
   <section className='section' id='projects'>
     <Reveal effect='fade-in'>
       <h2 className='section__title'>Projects</h2>
+      <Project video={eovisVideo} {...EOVIS_COPY} />
     	<Project video={grahamScanVideo} {...GRAHAM_SCAN_COPY} />
       <Project video={scheduleMakerVideo} {...SCHEDULE_MAKER_COPY} />
     </Reveal>
