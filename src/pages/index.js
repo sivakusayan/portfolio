fc6428a8ed50ac768db19df4998a858a36282e9a
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-02-11 21:49:41 -0500

    Fixed issue with gatsby build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import LazyLoad from 'vanilla-lazyload';
-
 import grahamScanVideo from '../assets/videos/grahamScanPreview.min.mp4';
 import scheduleMakerVideo from '../assets/videos/scheduleMakerPreview.min.mp4';
 
@@ -14,19 +12,31 @@ import Footer from '../sections/Footer';
 import GRAHAM_SCAN_COPY from '../__copy__/projects/GRAHAM_SCAN_COPY';
 import SCHEDULE_MAKER_COPY from '../__copy__/projects/SCHEDULE_MAKER_COPY';
 
+// Conditionally import LazyLoad to bypass gatsby
+// build problems.
+try {
+  // Var to not get block scoped
+  var LazyLoad = require('vanilla-lazyload').default;
+} catch (e) {
+  console.log('We are not in a browser environment.')
+}
+
+
 class IndexPage extends React.Component {
   componentDidMount() {
     // Instantiate lazyload after mount so program can
     // properly hook onto components
-    const myLazyLoad = new LazyLoad({
-      elements_selector: ".lazy",
-      // Fix for chrome data saver not letting videos autoplay
-      // https://stackoverflow.com/questions/47638344/muted-autoplay-in-chrome-still-not-working
-      callback_load: (video) => {
-        video.muted = true;
-        video.play();
-      },
-    });
+    if (LazyLoad) {
+      const myLazyLoad = new LazyLoad({
+        elements_selector: ".lazy",
+        // Fix for chrome data saver not letting videos autoplay
+        // https://stackoverflow.com/questions/47638344/muted-autoplay-in-chrome-still-not-working
+        callback_load: (video) => {
+          video.muted = true;
+          video.play();
+        },
+      });
+    }
   }
   render() {
     return (
