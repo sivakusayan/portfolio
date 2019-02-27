7b3cfc50d6726116b13a487f38f4a974f4c0a30c
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-02-26 22:21:37 -0500

    Replaced header animation. Buttons now have hover effect

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -4,13 +4,11 @@ import Reveal from 'react-reveal/Reveal';
 const Header = () => (
   <header className='header'>
     <div className='header__content'>
-      <Reveal effect='fade-in'>
-        <div className='header__text'>
-          <h1 className='header__title'>Hi. <br/> I'm Sayan.</h1>
-          <p className='header__blurb'>I make fast, responsive, and maintainable websites. Ready to make something amazing?</p>
-        </div>
-        <a className='btn btn--header' href='#projects'>See my Work</a>
-      </Reveal>
+      <div className='header__text'>
+        <h1 className='header__title'>Hi. <br/> I'm Sayan.</h1>
+        <p className='header__blurb'>I make fast, responsive, and maintainable websites. Ready to make something amazing?</p>
+      </div>
+      <a className='btn btn--header' href='#projects'>See my Work</a>
     </div>
   </header>
 );
