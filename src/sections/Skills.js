ca5fc29b8640d8afa315b451d2ba050565750cc8
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-03-12 10:41:46 -0400

    Finished markup for skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,31 +1,24 @@
 import React from 'react';
 
+import Skill from '../components/Skill';
 import SKILLS_COPY from '../__copy__/SKILLS_COPY';
+import toTitleCase from '../utils/toTitleCase';
+
+console.log(SKILLS_COPY)
+console.log(typeof SKILLS_COPY)
 
 const Skills = () => (
   <section className='section' id='skills'>
     <h2 className='section__title'>Skills</h2>
     <ul className='skills-container'>
-      <li className='skills'>
-        <p className='skills__category'>Frontend</p>
-        <ul className='skills__list'></ul>
+      {Object.keys(SKILLS_COPY).map(category => (
+        <li className='skills' key={category}>
+          <p className='skills__category'>{toTitleCase(category)}</p>
+          <ul className='skills__list'>
+            {SKILLS_COPY[category].map(skill => <Skill skill={skill} />)}
+          </ul>
       </li>
-      <li className='skills'>
-        <p className='skills__category'>Backend</p>
-        <ul className='skills__list'></ul>
-      </li>
-      <li className='skills'>
-        <p className='skills__category'>Testing</p>
-        <ul className='skills__list'></ul>
-      </li>
-      <li className='skills'>
-        <p className='skills__category'>Deployment</p>
-        <ul className='skills__list'></ul>
-      </li>
-      <li className='skills'>
-        <p className='skills__category'>Misc</p>
-        <ul className='skills__list'></ul>
-      </li>
+      ))}
     </ul>
   </section>
 )
