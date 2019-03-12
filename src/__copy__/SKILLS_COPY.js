16736320cea909088d2b795f5755c68408adaead
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-03-12 10:16:17 -0400

    Added skills copy

---
+++ src/__copy__/SKILLS_COPY.js
@@ -0,0 +1,42 @@
+// Note that when adding icons for each skills in the assets folder,
+// you must set the entire name to lowercase.
+// 
+// Examples: 
+// React   -> react.svg
+// SASS    -> sass.svg
+// Node.js -> node.js.svg
+//
+// However, the name you include here will be the name on the website.
+
+const SKILLS_COPY = {
+  frontend: [
+    'React',
+    'Redux',
+    'JavaScript',
+    'SASS',
+    'CSS',
+    'HTML5',
+  ],
+  backend: [
+    'Node.js',
+    'Express.js',
+    'MongoDB',
+    'MySQL'
+  ],
+  testing: [
+    'Jest',
+    'Enzyme',
+  ],
+  deployment: [
+    'Heroku',
+    'Netlify',
+  ],
+  misc: [
+    'Github',
+    'Webpack',
+    'Python',
+    'Java',
+  ],
+};
+
+export default SKILLS_COPY;
