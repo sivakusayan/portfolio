82a490dce2c8450061d7cf325726326875bf37c3
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-03-20 18:08:25 -0400

    Menu now pops up only after user is past header

--- src/sections/Navigation.js
+++ src/sections/Navigation.js
@@ -5,14 +5,33 @@ import HamburgerIcon from '../components/HamburgerIcon';
 
 class Navigation extends Component {
   state = {
+    // Whether the menu is hidden or not
     isOpen: false,
+    // Whether the button to open the menu is
+    // hidden or not. This is false while the
+    // user is in the header.
+    isClickable: false,
   }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', () => {
+      // We want the menu button to come out when user is no longer
+      // in the header landing page.
+      const isClickable = window.scrollY > window.innerHeight;
+      // Check if values are different so we don't
+      // flood the browser with needless updates
+      if (isClickable !== this.state.isClickable) {
+        this.setState({ isClickable });
+      }
+    })
+  }
+
   toggleOpen = () => this.setState((prevState) => ({
     isOpen: !prevState.isOpen,
   }))
 
   render() {
-    const { isOpen } = this.state
+    const { isOpen, isClickable } = this.state
     return (
       <>
         {/* Darkens screen while navigation menu is open */}
@@ -21,7 +40,11 @@ class Navigation extends Component {
           onClick={this.toggleOpen}
         />
         {/* Actual navigation menu */}
-        <div className={`navigation ${isOpen ? 'isOpen' : ''}`}>
+        <div 
+          className={
+            `navigation ${isOpen ? 'isOpen' : ''} ${isClickable ? '' : 'hide'}`
+          }
+        >
           <button 
             onClick={this.toggleOpen}
             className='btn btn--navigation'
