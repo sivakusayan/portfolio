d57b5cd41b7caa25ca146a9fab8d679dfcb564fa
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-03-26 00:22:24 -0400

    Made navigation menu responsive on bigger screens

--- src/__copy__/projects/EOVIS_COPY.js
+++ src/__copy__/projects/EOVIS_COPY.js
@@ -1,5 +1,5 @@
 const EOVIS_COPY = {
-  title: 'EOVIS',
+  title: 'EOVis',
   subtitle: 'An interactive map displaying natural disasters.',
   description: 'Built with the MERN stack, the application fetches data from an API to create an ever growing database of events. Using this data, a map of events is generated for the user to explore. If they like, the user can also search for events by location, date, and category.',
   technologies: ['React', 'Redux', 'Mapbox', 'Node.js'],
