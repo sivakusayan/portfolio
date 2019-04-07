6813562e8999537e49219800f108ab3432dbc25f
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-04-07 16:54:31 -0400

    Restyled skills section

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -6,17 +6,19 @@ import toTitleCase from '../utils/toTitleCase';
 
 const Skills = () => (
   <section className='section' id='skills-section'>
-    <h2 className='section__title'>Skills</h2>
-    <ul className='skills-container'>
-      {Object.keys(SKILLS_COPY).map(category => (
-        <li className='skills' key={category}>
-          <p className='skills__category'>{toTitleCase(category)}</p>
-          <ul className='skills__list'>
-            {SKILLS_COPY[category].map(skill => <Skill skill={skill} />)}
-          </ul>
-      </li>
-      ))}
-    </ul>
+    <div className="section__content">
+      <h2 className='section__title'>Skills</h2>
+      <ul className='skills-container'>
+        {Object.keys(SKILLS_COPY).map(category => (
+          <li className='skills' key={category}>
+            <p className='skills__category'>{toTitleCase(category)}</p>
+            <ul className='skills__list'>
+              {SKILLS_COPY[category].map(skill => <Skill skill={skill} />)}
+            </ul>
+        </li>
+        ))}
+      </ul>
+    </div>
   </section>
 )
 
