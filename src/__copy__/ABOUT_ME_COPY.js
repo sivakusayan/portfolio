150949276e4b2275f3db58a4e4e2c3267e25aec4
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-04-17 12:51:49 -0400

    Updated about me

--- src/__copy__/ABOUT_ME_COPY.js
+++ src/__copy__/ABOUT_ME_COPY.js
@@ -1,6 +1,6 @@
-const firstParagraph = "I'm Sayan Sivakumaran, and I am a developer that makes responsive, cross-browser websites and applications. As someone who likes to stay organized, clean and maintainable code is one of my top priorities.";
+const firstParagraph = "I'm Sayan Sivakumaran, and I am a developer who makes websites that are maintainable, responsive, and cross-browser compatible. Using technologies such as JavaScript and React, I can create anything from a single page application (SPA) to something more involved like a full-stack app.";
 
-const secondParagraph = "One of my key mantras is to never stop growing. That's why I am currently trying to take my CSS to the next level (an often neglected skill for something that can quickly become complex). Accessibility is another thing I started diving into, as I believe the web should be as widely usable as possible. When I'm not coding, you can find me working on my cooking skills or gushing over food chemistry."; 
+const secondParagraph = "I am currently going through Keith J. Grant's 'CSS in Depth', as I believe writing non-hacky, maintainable CSS is an often important yet unrecognized skill. I am also working on my UX design, as I want to make sure that my applications are as easy and convenient for the user as possible. When I'm not working on some code, you can find me enjoying a nice Stephen King novel or improving my skills in the kitchen."; 
 
 const ABOUT_ME = [firstParagraph, secondParagraph];
 
